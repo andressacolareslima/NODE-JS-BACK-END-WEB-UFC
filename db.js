3107ca508917bd77995718c446bd0cf90f0e5b66
Andressa Colares <andressacolares133@gmail.com>
2024-09-04 18:30:29 -0300

    subindo ultimas alterações

--- db.js
+++ db.js
@@ -6,9 +6,9 @@ const uri = process.env.MONGO_URI;
 const connectDB = async () => {
     try {
         await mongoose.connect(uri);
-        console.log('Conectado ao MongoDB Atlas com sucesso');
+        console.log('Conectado ao MongoDB com sucesso');
     } catch (error) {
-        console.error('Erro ao conectar ao MongoDB Atlas:', error.message);
+        console.error('Erro ao conectar ao MongoDB:', error.message);
         process.exit(1);
     }
 };
